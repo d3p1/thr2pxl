1766d45db9b6bb7123dbc4433e2d0f1726ff8c40
C. M. de Picciotto <d3p1@d3p1.dev>
2025-06-14 19:01:01 +0000

    refactor: add abstract entity implementation [#1]

---
+++ src/core/app/abstract-entity.ts
@@ -0,0 +1,59 @@
+/**
+ * @description Abstract entity
+ * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
+ * @note        Main entities of this implementation will need to load
+ *              a model to work. The idea behind this class is to
+ *              encapsulate the common behavior of these entities
+ */
+import * as THREE from 'three'
+import ModelLoaderManager from '../lib/model-loader-manager.ts'
+
+export default abstract class AbstractEntity {
+  /**
+   * @type {THREE.Mesh<THREE.BufferGeometry, THREE.Material> | null}
+   */
+  mesh: THREE.Mesh<THREE.BufferGeometry, THREE.Material> | null = null
+
+  /**
+   * @type {string}
+   */
+  readonly #modelUrl: string
+
+  /**
+   * @type {ModelLoaderManager}
+   */
+  #modelLoaderManager: ModelLoaderManager
+
+  /**
+   * Constructor
+   *
+   * @param {string}             modelUrl
+   * @param {ModelLoaderManager} modelLoaderManager
+   */
+  protected constructor(
+    modelUrl: string,
+    modelLoaderManager: ModelLoaderManager,
+  ) {
+    this.#modelUrl = modelUrl
+    this.#modelLoaderManager = modelLoaderManager
+  }
+
+  /**
+   * Load entity/model
+   *
+   * @returns {Promise<void>}
+   */
+  async load(): Promise<void> {
+    this.mesh = await this.#modelLoaderManager.loadMeshFromModel(this.#modelUrl)
+  }
+
+  /**
+   * Dispose
+   *
+   * @returns {void}
+   */
+  dispose(): void {
+    this.mesh?.geometry.dispose()
+    this.mesh?.material.dispose()
+  }
+}
