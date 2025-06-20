523ad53d76470c31c1029fe07286a47ea0488e0a
C. M. de Picciotto <d3p1@d3p1.dev>
2025-06-20 01:50:42 +0000

    docs: improve effect and model for demo page [#1][#2]

--- www/app/component/Lib.tsx
+++ www/app/component/Lib.tsx
@@ -12,18 +12,34 @@ export default function Lib() {
     new Thr2pxl({
       model: {
         src: {
-          highPoly: '/thr2pxl/media/images/lib/ship.glb',
-          lowPoly: '/thr2pxl/media/images/lib/ship.simplified.glb',
+          highPoly: '/thr2pxl/media/images/lib/queen/queen.glb',
+          lowPoly: '/thr2pxl/media/images/lib/queen/queen.simplified.glb',
         },
-        width: 1200,
-        height: 800,
+        width: window.innerWidth,
+        height: window.innerHeight,
         camera: {
           position: {
-            x: 1.5,
-            y: 0,
-            z: 10,
+            x: 4,
+            y: 6,
+            z: 4,
           },
         },
+        point: {
+          size: 5,
+          motion: {
+            frequency: 0.15,
+            strength: 2.5,
+            ratio: 0.25,
+            lifeDecay: 0.6,
+          },
+        },
+      },
+      pointer: {
+        strength: 0.2,
+        minRad: 1,
+        maxRad: 2,
+        pulseStrength: 0.2,
+        pulseFrequency: 1,
       },
       loader: {
         dracoUrl: '/thr2pxl/js/draco/',
