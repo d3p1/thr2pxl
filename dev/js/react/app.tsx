ef0ad60b43d7321456f1ec6e17df71e5d218b8f3
C. M. de Picciotto <d3p1@d3p1.dev>
2026-01-02 05:47:25 +0000

    refactor: improve import syntax [#1]

--- dev/js/react/app.tsx
+++ dev/js/react/app.tsx
@@ -3,6 +3,6 @@
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
 import {createRoot} from 'react-dom/client'
-import Lib from './app/Lib.tsx'
+import Lib from './app/Lib.jsx'
 
 createRoot(document.getElementById('root')!).render(<Lib />)
--- src/react/index.ts
+++ src/react/index.ts
@@ -2,6 +2,6 @@
  * @description Index
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
-export {default as Thr2Pxl} from './components/Thr2Pxl'
-export {default as useThr2Pxl} from './hooks/useThr2Pxl'
+export {default as Thr2Pxl} from './components/Thr2Pxl.jsx'
+export {default as useThr2Pxl} from './hooks/useThr2Pxl.js'
 export * from './types'
