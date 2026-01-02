434258076894e10386aa39e5f9941dc5a08c3343
C. M. de Picciotto <d3p1@d3p1.dev>
2026-01-02 05:18:06 +0000

    docs: solve window not defined for library component on demo site [#8]

--- www/app/page.tsx
+++ www/app/page.tsx
@@ -2,7 +2,9 @@
  * @description Home page
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
-import Lib from '@/app/component/Lib'
+import dynamic from 'next/dynamic'
+
+const Lib = dynamic(() => import('@/app/component/Lib'), {ssr: false})
 
 export default function HomePage() {
   return <Lib />
