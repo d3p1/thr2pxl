61935747fcbf0e86a0dd1116711d33f921545e23
C. M. de Picciotto <d3p1@d3p1.dev>
2026-01-02 05:24:44 +0000

    docs: solve hydratation error on demo site [#8]

--- www/app/component/Lib.tsx
+++ www/app/component/Lib.tsx
@@ -7,70 +7,72 @@
 import {useThr2Pxl} from '@d3p1/thr2pxl/react'
 
 export default function Lib() {
-  useThr2Pxl({
-    models: {
-      0: {
-        src: {
-          highPoly: '/thr2pxl/media/images/lib/queen/queen.glb',
-          lowPoly: '/thr2pxl/media/images/lib/queen/queen.simplified.glb',
-        },
-        width: window.innerWidth * 0.55,
-        height: window.innerHeight * 0.75,
-        camera: {
-          position: {
-            x: 0,
-            y: 0,
-            z: 6,
+  if (typeof window !== 'undefined') {
+    useThr2Pxl({
+      models: {
+        0: {
+          src: {
+            highPoly: '/thr2pxl/media/images/lib/queen/queen.glb',
+            lowPoly: '/thr2pxl/media/images/lib/queen/queen.simplified.glb',
+          },
+          width: window.innerWidth * 0.55,
+          height: window.innerHeight * 0.75,
+          camera: {
+            position: {
+              x: 0,
+              y: 0,
+              z: 6,
+            },
+          },
+          point: {
+            size: 5,
+            motion: {
+              frequency: 0.1,
+              strength: 1.5,
+              ratio: 0.25,
+              lifeDecay: 0.2,
+            },
           },
         },
-        point: {
-          size: 5,
-          motion: {
-            frequency: 0.1,
-            strength: 1.5,
-            ratio: 0.25,
-            lifeDecay: 0.2,
+        769: {
+          src: {
+            highPoly: '/thr2pxl/media/images/lib/queen/queen.glb',
+            lowPoly: '/thr2pxl/media/images/lib/queen/queen.simplified.glb',
+          },
+          width: window.innerWidth * 0.25,
+          height: window.innerHeight * 0.85,
+          camera: {
+            position: {
+              x: 0,
+              y: 0,
+              z: 4,
+            },
+          },
+          point: {
+            size: 5,
+            motion: {
+              frequency: 0.1,
+              strength: 1.5,
+              ratio: 0.25,
+              lifeDecay: 0.2,
+            },
           },
         },
       },
-      769: {
-        src: {
-          highPoly: '/thr2pxl/media/images/lib/queen/queen.glb',
-          lowPoly: '/thr2pxl/media/images/lib/queen/queen.simplified.glb',
-        },
-        width: window.innerWidth * 0.25,
-        height: window.innerHeight * 0.85,
-        camera: {
-          position: {
-            x: 0,
-            y: 0,
-            z: 4,
-          },
-        },
-        point: {
-          size: 5,
-          motion: {
-            frequency: 0.1,
-            strength: 1.5,
-            ratio: 0.25,
-            lifeDecay: 0.2,
-          },
-        },
+      pointer: {
+        strength: 0.2,
+        minRad: 1,
+        maxRad: 2,
+        pulseStrength: 0.2,
+        pulseFrequency: 1,
       },
-    },
-    pointer: {
-      strength: 0.2,
-      minRad: 1,
-      maxRad: 2,
-      pulseStrength: 0.2,
-      pulseFrequency: 1,
-    },
-    loader: {
-      dracoUrl: '/thr2pxl/js/draco/',
-    },
-    containerSelector: '#thr2pxl',
-    isDebugging: true,
-  })
+      loader: {
+        dracoUrl: '/thr2pxl/js/draco/',
+      },
+      containerSelector: '#thr2pxl',
+      isDebugging: true,
+    })
+  }
 
   return (
     <div
--- www/app/page.tsx
+++ www/app/page.tsx
@@ -2,9 +2,7 @@
  * @description Home page
  * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
  */
-import dynamic from 'next/dynamic'
-
-const Lib = dynamic(() => import('@/app/component/Lib'), {ssr: false})
+import Lib from '@/app/component/Lib'
 
 export default function HomePage() {
   return <Lib />
